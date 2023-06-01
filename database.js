49254618f814e1d1dbfc71745fdd8c4fc397f182
chaihelloworld <ckdu22.5.9@hotmail.com>
2023-06-01 23:58:48 +0700

    update env

--- database.js
+++ database.js
@@ -3,14 +3,14 @@ require('dotenv').config()
 
 
 var conn = mysql.createConnection({
-  // host: process.env.IP, // Replace with your host name
-  // user: process.env.USERNAME,      // Replace with your database username
-  // password: process.env.PASSWORD,      // Replace with your database password
-  // database: process.env.DB // // Replace with your database Name
-  host:'128.199.71.21', // Replace with your host name
-  user: 'admin',      // Replace with your database username
-  password: 'admin.password',      // Replace with your database password
-  database: 'web_p' // // Replace with your database Name
+  host: process.env.IP, // Replace with your host name
+  user: process.env.USERNAME,      // Replace with your database username
+  password: process.env.PASSWORD,      // Replace with your database password
+  database: process.env.DB // // Replace with your database Name
+  // host:'128.199.71.21', // Replace with your host name
+  // user: 'admin',      // Replace with your database username
+  // password: 'admin.password',      // Replace with your database password
+  // database: 'web_p' // // Replace with your database Name
   // database: 'resource_information' // // Replace with your database Name
 }); 
  
